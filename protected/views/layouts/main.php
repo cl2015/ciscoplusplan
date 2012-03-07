7eac8bcb6ba7ce925c18b239853dc39a4866c77d
Li He <cranelee@gmail.com>
2012-03-07 17:46:31 +0800

    add nav

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,8 +30,10 @@
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+
 				array('label'=>'演示入口-1', 'url'=>array('/site/begin')),
-				array('label'=>'演示入口-2 Ordinary', 'url'=>array('/user/loading')),
+				array('label'=>'report', 'url'=>array('/report')),
+				//array('label'=>'演示入口-2 Ordinary', 'url'=>array('/user/loading')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
