290da1806baea945c1341d4a911c9243af85aac0
Li He <cranelee@gmail.com>
2012-03-08 10:12:14 +0800

    for merge

--- protected/views/reginfo/confirmation.php
+++ protected/views/reginfo/confirmation.php
@@ -1,7 +1,11 @@
 <h1>Confirmation</h1>
 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/step3.png" /></br>
-this is confirmation page.
-<a href="javascript:alert('This flow is over ,bye ^_^');">click me</a>
+<p>
+Thanks for registering the Cisco Plus 2012 event , your registration ID is W000001 ,etc.
 </br>
+
+Please print and save this confirmation for your records.  This information has also been sent to your email at cranelee@gmail.com.  If you do not receive the registration confirmation , please check your
+spam or junk folders first; if you still have not received your confirmation email, please contact Hotline Number.</br>
+</p>
 <?php echo CHtml::link(CHtml::encode("click here view profile"), array('user/view', 'id'=>1 )); ?>
 </br>
