e5de8771a8d0455d88e4320b0a06e933328466f0
leftarrow <cranelee@gmail.com>
2012-03-17 18:05:54 +0800

    fcu去rhj掉mmb删除alc功能

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
 						'users'=>array('@'),
 				),
 				array('allow', // allow admin user to perform 'admin' and 'delete' actions
-						'actions'=>array('create','update','admin','delete','view','attending','emailreg','ordinaryUpdate','index'),
+						'actions'=>array('create','update','admin','view','attending','emailreg','ordinaryUpdate','index'),
 						'users'=>array('admin'),
 				),
 				array('deny',  // deny all users
