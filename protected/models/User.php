e2e72202550ac2a49bb04a4c51442da12ed079ac
leftarrow <cranelee@gmail.com>
2012-03-17 16:39:55 +0800

    /^(.)*[\x7f-\xff]+(.)*$/
    /^(.)*[\x7f-\xff]+(.)*$/

--- protected/models/User.php
+++ protected/models/User.php
@@ -87,7 +87,7 @@ class User extends TrackStarActiveRecord {
 		);
 		if(Yii::app()->language=='zh_cn'){
 			$rules[]=array('full_name', 'required','on' =>'update');
-			$rules[]=array('full_name,organisation,city,department','match','pattern'=>"/^[a-zA-Z0-9 ]*[\x7f-\xff]+[a-zA-Z0-9 ]*$/",'message'=>'必须包含中文','on'=>'update');
+			$rules[]=array('full_name,organisation,city,department','match','pattern'=>"/^(.)*[\x7f-\xff]+(.)*$/",'message'=>'必须包含中文','on'=>'update');
 		}else{
 			$rules[]=array('first_name, last_name', 'required','on' =>'update');
 		}
