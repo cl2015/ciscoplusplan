37876fff817f5a65fd33fd28fc4f7826b05b8fed
leftarrow <cranelee@gmail.com>
2012-03-23 20:58:45 +0800

    tracking

--- themes/zh_cn/views/reginfo/ordinaryConfirmation.php
+++ themes/zh_cn/views/reginfo/ordinaryConfirmation.php
@@ -1,6 +1,48 @@
 <?php if(Yii::app()->language=='zh_cn'){?>
 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/3.png"/>
+<?php if($reginfo['is_online']=='0'){?>
+<! --参加在线活动 -->
+<script type="text/javascript">
+        ACTracker.track({"type": "event", "action": "在线活动完成量"});
+</script>
 <?php }else{?>
+<! --参加现场活动 -->
+<script type="text/javascript">
+        ACTracker.track({"type": "event", "action": "现场活动继续"});
+</script>
+<?php }?>
+
+<?php if($reginfo['payment_type']=='1'){?>
+<!-- 选择现场支付 -->
+<script type="text/javascript">
+        ACTracker.track({"type": "event", "action": "线下支付完成数量"});
+</script>
+<?php }?>
+<?php }else{?>
+<?php if($reginfo['is_online']=='0'){?>
+<! --参加在线活动 -->
+<script type="text/javascript">
+        ACTracker.track({"type": "event", "action": "E在线活动完成量"});
+</script>
+<?php }else{?>
+<! --参加现场活动 -->
+<script type="text/javascript">
+        ACTracker.track({"type": "event", "action": "E现场活动继续"});
+</script>
+<?php }?>
+
+<?php if($reginfo['payment_type']=='1'){?>
+<!-- 选择现场支付 -->
+<script type="text/javascript">
+        ACTracker.track({"type": "event", "action": "E线下支付完成数量"});
+</script>
+<?php }?>
 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/3_en.jpg"/>
 <?php }?>
+
 <?php echo $this->renderPartial('_confirmation', array('model'=>$model,'reginfo'=>$reginfo)); ?>
+
+<iframe width=0 height=0>
+<img src="http://apps.acs86.com/h.ashx?o=sub&id=917&em=<?php echo $model->email?>"/></iframe>
+<script charset="utf-8" src="http://apps.acs86.com/h.ashx?o=sub&id=917&em=<?php echo $model->email?>"></script>
+
