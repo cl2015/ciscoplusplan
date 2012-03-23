ffc299907dde0c9b99772c2db2a360f1abaafc6a
leftarrow <cranelee@gmail.com>
2012-03-23 17:38:08 +0800

    tracking

--- themes/zh_cn/views/reginfo/attendeeConfirmation.php
+++ themes/zh_cn/views/reginfo/attendeeConfirmation.php
@@ -1,25 +1,43 @@
 <?php if(Yii::app()->language=='zh_cn'){?>
 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/3.png"/>
-<?php }else{?>
-<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/3_en.jpg"/>
-<?php }?>
-
-<?php echo $this->renderPartial('_confirmation', array('model'=>$model,'reginfo'=>$reginfo)); ?>
 <?php if($reginfo['is_online']=='0'){?>
 <! --参加在线活动 -->
 <script type="text/javascript">
-        ACTracker.track({"type": "event", "action": "注册成功1"});
+        ACTracker.track({"type": "event", "action": "在线活动完成量"});
 </script>
 <?php }else{?>
 <! --参加现场活动 -->
 <script type="text/javascript">
-        ACTracker.track({"type": "event", "action": "注册成功2"});
+        ACTracker.track({"type": "event", "action": "现场活动继续"});
 </script>
 <?php }?>
 
 <?php if($reginfo['payment_type']=='1'){?>
 <!-- 选择现场支付 -->
 <script type="text/javascript">
-        ACTracker.track({"type": "event", "action": "现场支付"});
+        ACTracker.track({"type": "event", "action": "线下支付完成数量"});
 </script>
 <?php }?>
+<?php }else{?>
+<?php if($reginfo['is_online']=='0'){?>
+<! --参加在线活动 -->
+<script type="text/javascript">
+        ACTracker.track({"type": "event", "action": "E在线活动完成量"});
+</script>
+<?php }else{?>
+<! --参加现场活动 -->
+<script type="text/javascript">
+        ACTracker.track({"type": "event", "action": "E现场活动继续"});
+</script>
+<?php }?>
+
+<?php if($reginfo['payment_type']=='1'){?>
+<!-- 选择现场支付 -->
+<script type="text/javascript">
+        ACTracker.track({"type": "event", "action": "E线下支付完成数量"});
+</script>
+<?php }?>
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/3_en.jpg"/>
+<?php }?>
+
+<?php echo $this->renderPartial('_confirmation', array('model'=>$model,'reginfo'=>$reginfo)); ?>
--- themes/zh_cn/views/reginfo/ordinaryConfirmation.php
+++ themes/zh_cn/views/reginfo/ordinaryConfirmation.php
@@ -4,7 +4,3 @@
 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/3_en.jpg"/>
 <?php }?>
 <?php echo $this->renderPartial('_confirmation', array('model'=>$model,'reginfo'=>$reginfo)); ?>
-
-<script type="text/javascript">
-        ACTracker.track({"type": "event", "action": "注册成功"});
-</script>
