18b77d8d5752f84c8ecd1baf0ab8f0ba67a4548c
李贺left <he.lee@163.com>
2012-03-31 12:03:16 +0800

    Update mike/sql1.sql

---
+++ mike/sql1.sql
@@ -0,0 +1,7 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
+insert into users (organisation,relation_with_cisco,full_name,job_title,department,mobile,email,province,city,am_name,am_id,am_mobile,am_department,rm_name,rm_id,od_name,od_id,diff,type_id,cc)values ('国家税务总局','客户','曹存义','员工','IT部门','13501081949','Ccy@chinatax.gov.cn','北京','北京','Runhong Wei','ruwei','13910992705','PL-North','Eddie Zhuo','bzhuo','Jerry Zhang','jerzhang','PL','1','ruwei@cisco.com');
