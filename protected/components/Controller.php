c509018acda3767cc359ec577138ebc9adb2e07c
leftarrow <cranelee@gmail.com>
2012-03-31 11:37:55 +0800

    for mike

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -15,6 +15,7 @@ class Controller extends CController
 	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
 	 */
 	public $menu=array();
+	
 	/**
 	 * @var array the breadcrumbs of the current page. The value of this property will
 	 * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
