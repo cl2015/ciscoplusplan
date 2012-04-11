67405c84fdc168182aa7da2f7ada0984af122d08
李贺left <he.lee@163.com>
2012-04-11 17:38:53 +0800

    Update mike/sql1.sql

--- mike/sql1.sql
+++ mike/sql1.sql
@@ -4,5 +4,6 @@
 /*!40101 SET NAMES utf8 */;
 
 
-insert into users (organisation,relation_with_cisco,full_name,job_title,department,mobile,email,province,city,am_name,am_id,am_mobile,am_department,rm_name,rm_id,od_name,od_id,diff,type_id,cc)values ('中登公司','客户','张静','总监/资深经理','IT部门','','jzhang@chinaclear.com.cn','北京','北京','Peter Wei','peterwei','CL','13901067827','Kevin Zheng','kbzheng','Guangyue Hai','guahai','CL','1','peterwei@cisco.com');
-
+insert into users (organisation,relation_with_cisco,full_name,job_title,department,mobile,email,province,city,am_name,am_id,am_mobile,am_department,rm_name,rm_id,od_name,od_id,diff,type_id,cc)values ('Volkswagen Group','客户','刘维','工程师','IT部门','13810260659','extern.wei.liu@volkswagen.com.cn','北京','北京','Grace Wu','kejwu','PL','13910080713','Frank Chen','wmchen','Jerry Zhang','jerzhang','PL','1','kejwu@cisco.com');
+insert into users (organisation,relation_with_cisco,full_name,job_title,department,mobile,email,province,city,am_name,am_id,am_mobile,am_department,rm_name,rm_id,od_name,od_id,diff,type_id,cc)values ('Volkswagen Group','客户','刘赓','项目经理','IT部门','18600093298','Geng.liu@volkswagen.com.cn','北京','北京','Grace Wu','kejwu','PL','13910080713','Frank Chen','wmchen','Jerry Zhang','jerzhang','PL','1','kejwu@cisco.com');
+insert into users (organisation,relation_with_cisco,full_name,job_title,department,mobile,email,province,city,am_name,am_id,am_mobile,am_department,rm_name,rm_id,od_name,od_id,diff,type_id,cc)values ('Vestas','客户','张斌元','员工','IT部门','13910856255','bebzh@vestas.com','北京','北京','Grace Wu','kejwu','PL','13910080713','Frank Chen','wmchen','Jerry Zhang','jerzhang','PL','1','kejwu@cisco.com');
