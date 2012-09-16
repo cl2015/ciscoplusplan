d28a124be7a1f8169d9a2438a536233be9cfc9a1
leftarrow <he.lee@163.com>
2012-09-16 14:59:05 +0800

    关闭会议

--- themes/zh_cn/views/site/index.php
+++ themes/zh_cn/views/site/index.php
@@ -1,10 +1,5 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
-<?php /*
-<p>对不起，我们的会议注册功能已经关闭。如您仍有参会需求，请发送电子邮件至：<a href="mailto:gc_cisco_plus@external.cisco.com">gc_cisco_plus@external.cisco.com</a> 或拨打热线电话<span style="color:red">400－156－3788</span>，我们将会有专人接待您。</p>
+<p>对不起，我们的会议注册功能已经关闭。如您仍有参会需求，请发送电子邮件至：<a href="mailto:gc_cisco_plus@external.cisco.com">gc_cisco_plus@external.cisco.com</a> 或拨打热线电话<span style="color:red">400-890-9901</span>，我们将会有专人接待您。</p>
 <p>热线工作时间：周一至周五 09:00～18:00。</p>
-<p>感谢您对<span style="color:red">CiscoPlus</span>的关注。
-</p> 
-<p><span style="color:red">Cisco Plus</span>大中华区北京站会务组</p>
-<p>
-	</p>
-*/?>
+ <p>感谢您对<span style="color:red">CiscoPlus</span>的关注。
+ <p><span style="color:red">Cisco Plus</span>大中华区活动会务组</p>
