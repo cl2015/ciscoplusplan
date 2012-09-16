43de0ce8deaa173cb9f8eef201b20c8bed4ff9e0
leftarrow <he.lee@163.com>
2012-09-16 15:02:29 +0800

    找回密码

--- protected/views/user/forgetPasswordEmail.php
+++ protected/views/user/forgetPasswordEmail.php
@@ -3,7 +3,7 @@
 <img src="http://223.4.134.123/images/mainBg.jpg" />
 <p>
 	尊敬的<?php echo $model->full_name;?><br /> 
-	感谢您注册Cisco Plus大中华区北京站活动！<br />
+	感谢您注册Cisco Plus大中华区广州站活动！<br />
 </p>
 <p>
 <?php if(!($model->type_id== 4 && $reginfo->is_online==0)){?>	
@@ -24,16 +24,16 @@
 	
 <?php if(!($model->type_id== 4 && $reginfo->is_online==0)){?>	
 	<p>
-	会议日期：2012 年 5 月 22-23 日 <br />
+	会议日期：2012年9月20日 <br />
 	签到时间：早八点开始<br />
-	会议地点：国家会议中心<br />
+	会议地点：广州白云万达希尔顿酒店<br />
 	如欲了解更多会议详情，请访问<a href="http://www.ciscopluschina.com">Cisco Plus 主页</a><br/>
 </p>
 <?php }?>
 <p>
 	我们期待着您的参与！ <br/>
 	问题或建议,请发送电子邮件至:gc_cisco_plus@external.cisco.com<br/>
-	或致电: 400-156-3788
+	或致电: 400-890-9901
 <p>
 <p>
 	<b>Cisco Plus大中华区活动会务组</b>
@@ -66,15 +66,15 @@ This email address and password were used to login the active site of the Genera
 </p>
 <?php if(!($model->type_id== 4 && $reginfo->is_online==0)){?>	
 <p>
-Event Date:           Mar 22-23, 2012<br />
-Sign in time:         Start from 8:00 AM<br />
-Venue:                China National Convention Center,Beijing, PRC<br />
+<!-- Event Date:           Mar 22-23, 2012<br /> -->
+<!-- Sign in time:         Start from 8:00 AM<br /> -->
+<!-- Venue:                China National Convention Center,Beijing, PRC<br /> -->
 For more conference details, please visit the <a href="http://www.ciscopluschina.com"> Cisco Plus Home Page</a>.<br />
 We look forward to seeing you!
 </p>
 <?php }?>
 <p> 
-If you have any questions concerning your registration, please mail to: gc_cisco_plus@external.cisco.com  or call: 400-156-3788<br />
+If you have any questions concerning your registration, please mail to: gc_cisco_plus@external.cisco.com  or call: 400-890-9901<br />
 </p> 
 <p>  
 Cisco Plus 2012 Greater China Event Committee Team
