cfe4dd94d37c6e16ae65bf7327c7755d895a4c35
leftarrow <he.lee@163.com>
2012-10-15 16:42:51 +0800

    开放注册

--- protected/controllers/ReginfoController.php
+++ protected/controllers/ReginfoController.php
@@ -175,10 +175,10 @@ class ReginfoController extends Controller {
         if (isset($_POST['Reginfo'])) {
             $model->attributes = $_POST['Reginfo'];
             //close onsite login
-            if(!$model->is_online == 0){
-            	$this->redirect(array('site/index'));
-				Yii::app()->end();
-            }
+//             if(!$model->is_online == 0){
+//             	$this->redirect(array('site/index'));
+// 				Yii::app()->end();
+//             }
             
             if ($model->save()) {
                 if ($model->is_online == 0) {
